a5afc7204f7ff9c134ce6bfea3bea61ab2dfa4c2
Guilherme França <guilherme_capivari@hotmail.com>
2023-04-26 21:06:21 -0300

    Estrutura inicial

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss'],
+})
+export class TimerComponent implements OnInit, OnDestroy {
+  minutes: number = 25;
+  seconds: number = 0;
+  isRunning: boolean = false;
+  timerId?: any;
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  startTimer() {
+    if (this.isRunning) {
+      clearInterval(this.timerId);
+    } else {
+      this.timerId = setInterval(() => {
+        if (this.seconds === 0) {
+          if (this.minutes === 0) {
+            clearInterval(this.timerId);
+            this.isRunning = false;
+            return;
+          } else {
+            this.minutes--;
+            this.seconds = 59;
+          }
+        } else {
+          this.seconds--;
+        }
+      }, 1000);
+    }
+
+    this.isRunning = !this.isRunning;
+  }
+
+  resetTimer() {
+    clearInterval(this.timerId);
+    this.isRunning = false;
+    this.minutes = 25;
+    this.seconds = 0;
+  }
+}
